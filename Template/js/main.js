83450073116e0ff27f919fc10ff7f5945b0729aa
Diego Jimenez <djontiveros2728@gmail.com>
2025-03-06 12:26:06 -0600

    First D3Lab

--- Template/js/main.js
+++ Template/js/main.js
@@ -2,3 +2,19 @@
 *    main.js
 */
 
+var svg = d3.select("#chart-area").append("svg")
+  .attr("width", 400)
+  .attr("height", 400);
+
+var circle = svg.append("circle")
+  .attr("cx", 376)
+  .attr("cy", 250)
+  .attr("r", 70)
+  .attr("fill", "blue");
+
+var rect = svg.append("rect")
+  .attr("x", 50)
+  .attr("y", 50)
+  .attr("width", 100)
+  .attr("height", 75)
+  .attr("fill", "red");
