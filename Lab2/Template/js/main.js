a75e0916f7e37f38832724a55a3c780426269641
Diego Jimenez <djontiveros2728@gmail.com>
2025-03-11 10:40:37 -0600

    Lab2 Done

---
+++ Lab2/Template/js/main.js
@@ -0,0 +1,21 @@
+/*
+*    main.js
+*/
+
+var data = [25, 20, 15, 10, 5];
+
+var svg = d3.select("#chart-area").append("svg")
+  .attr("width", 400)
+  .attr("height", 400);
+
+var react = svg.selectAll("rect")
+  .data(data);
+
+react.enter()
+  .append("rect")
+  .attr("width", 40)
+  .attr("height", (d) => { return d })
+  .attr("x", (d, i) => { return i * 50 })
+  .attr("y", 100)
+  .attr("fill", "blue");
+
